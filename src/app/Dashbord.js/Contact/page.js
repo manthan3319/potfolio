e40beb96bd287cc46a3b70af3a1469d03309b5d8
manthan3319 <vaghasiyamanthan8@gmail.com>
2024-06-25 16:12:38 +0530

    change contact page

--- src/app/Dashbord.js/Contact/page.js
+++ src/app/Dashbord.js/Contact/page.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import { Title } from '../Title/page';
 import Link from 'next/link';
+
 export const Contact = () => {
     const links = [
         { href: 'https://www.linkedin.com/in/yourprofile', icon: 'fa-linkedin', name: "Linkedin" },
         { href: 'https://github.com/yourprofile', icon: 'fa-github', name: "Github" },
-        { href: 'https://wa.me/yourphonenumber', icon: 'fa-whatsapp', name: "whatsapp" },
-        
+        { href: 'https://wa.me/yourphonenumber', icon: 'fa-whatsapp', name: "Whatsapp" },
     ];
+
     return (
         <div className='lg:max-w-[1440px] xl:px-[90px] mx-auto m-auto px-[20px] lg:mt-[130px] bg-[#050709] lg:py-[50px] mt-[80px]'>
             <div>
                 <Title pram={{ title: 'Contact' }} />
             </div>
 
-            <div className='flex lg:flex-row flex-col lg:items-center gap-y-[80px]  lg:gap-[100px] lg:mt-[40px] '>
+            <div className='flex lg:flex-row flex-col lg:items-center gap-y-[80px] lg:gap-[100px] lg:mt-[40px]'>
                 <div className='flex flex-col lg:gap-[20px] gap-[30px]'>
                     <div className='flex gap-[20px] items-center'>
                         <div>
-                            <span className='bg-gradient w-[45px] h-[45px] inline-block text-center text-[25px] rounded-[50px] text-white py-[5px]'><i class="fa fa-phone" aria-hidden="true"></i></span>
+                            <span className='bg-gradient w-[45px] h-[45px] inline-block text-center text-[25px] rounded-[50px] text-white py-[5px]'>
+                                <i className="fa fa-phone" aria-hidden="true"></i>
+                            </span>
                         </div>
                         <div className='flex flex-col gap-y-[3px]'>
                             <p className='text-white font-sans text-[17px] font-medium'>Phone</p>
@@ -28,43 +31,49 @@ export const Contact = () => {
 
                     <div className='flex gap-[20px] items-center'>
                         <div>
-                            <span className='bg-gradient w-[45px] h-[45px] inline-block text-center text-[25px] rounded-[50px] text-white py-[5px]'> <i class="fa fa-map-marker" aria-hidden="true"></i></span>
+                            <span className='bg-gradient w-[45px] h-[45px] inline-block text-center text-[25px] rounded-[50px] text-white py-[5px]'>
+                                <i className="fa fa-map-marker" aria-hidden="true"></i>
+                            </span>
                         </div>
                         <div className='flex flex-col gap-y-[3px]'>
                             <p className='text-white font-sans text-[17px] font-medium'>Email</p>
-                            <Link href="./" className='text-white font-sans lg:text-[20px] text-[10px] font-bold inline-block'>vaghasiyamanthankumar@gmail.com</Link>
+                            <Link href="./" className='text-white font-sans lg:text-[20px] text-[10px] font-bold inline-block'>
+                                vaghasiyamanthankumar@gmail.com
+                            </Link>
                         </div>
                     </div>
 
                     <div className='flex gap-[20px] items-center'>
                         <div>
-                            <span className='bg-gradient w-[45px] h-[45px] inline-block text-center text-[25px] rounded-[50px] text-white py-[5px]'><i class="fa fa-phone" aria-hidden="true"></i></span>
+                            <span className='bg-gradient w-[45px] h-[45px] inline-block text-center text-[25px] rounded-[50px] text-white py-[5px]'>
+                                <i className="fa fa-phone" aria-hidden="true"></i>
+                            </span>
                         </div>
                         <div className='flex flex-col gap-y-[3px]'>
                             <p className='text-white font-sans text-[17px] font-medium'>Address</p>
-                            <Link href="./" className='text-white font-sans lg:text-[20px] text-[14px] font-bold inline-block max-w-[500px]'>1ST FLOOR B106, SHANTINAGAR SOCIETY HIRABAUG, SURAT CITY, GUJARAT, INDIA</Link>
+                            <Link href="./" className='text-white font-sans lg:text-[20px] text-[14px] font-bold inline-block max-w-[500px]'>
+                                1ST FLOOR B106, SHANTINAGAR SOCIETY HIRABAUG, SURAT CITY, GUJARAT, INDIA
+                            </Link>
                         </div>
                     </div>
                 </div>
 
                 <div className='mb-[60px]'>
                     <div>
-                        <div className='flex flex-col  gap-[20px]'>
+                        <div className='flex flex-col gap-[20px]'>
                             {links.map((link, index) => (
-                                <div className='flex flex-row   gap-[14px] items-center'>
-                                    <Link href={link.href} key={index} target="_blank" rel="noopener noreferrer"
+                                <div key={index} className='flex flex-row gap-[14px] items-center'>
+                                    <Link href={link.href} target="_blank" rel="noopener noreferrer"
                                         className='relative flex items-center justify-center border border-white-8c w-[35px] h-[35px] rounded-full p-2 transition duration-300 hover:bg-gradient2 hover:border-none'>
                                         <i className={`fa ${link.icon} text-white`} aria-hidden="true"></i>
                                     </Link>
-
                                     <p className='text-white font-sans text-[18px]'>{link.name}</p>
                                 </div>
-
                             ))}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
